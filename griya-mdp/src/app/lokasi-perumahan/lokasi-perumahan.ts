f625dbdfda3ae6e4a448fa7c6fbc6f29ecbcfbf8
NabilaSyahwalrani <nabilasyahwalrani_2327240103@mhs.mdp.ac.id>
2025-11-16 20:52:41 +0700

    Implementasi Home Component

--- griya-mdp/src/app/lokasi-perumahan/lokasi-perumahan.ts
+++ griya-mdp/src/app/lokasi-perumahan/lokasi-perumahan.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lokasi-perumahan',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './lokasi-perumahan.html',
-  styleUrl: './lokasi-perumahan.css',
+  styleUrl: './lokasi-perumahan.css'
 })
 export class LokasiPerumahan {
-
+  // Nanti bisa ditambahkan @Input() untuk menerima data dari parent
 }
